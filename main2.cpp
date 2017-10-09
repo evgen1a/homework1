f138fdf6bbe197250e52306f17fdcac177432d64
evgen1a <32650640+evgen1a@users.noreply.github.com>
2017-10-09 22:08:04 +0300

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main() {
+    setlocale (LC_CTYPE, "rus");
+	cout <<"Кособокова Евгения\n";
+	cout <<"Нижегородская область Кстовский район д.Афонино ул.Магистральная";
+	return 0;
+}
